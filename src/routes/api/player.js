ebbc67aa132a03e079551f730ced61750b2abae5
Kurtis Blankenship <BlankenshipKurtisA@JohnDeere.com>
2019-07-26 15:32:05 -0500

    refactoring then statements

--- src/routes/api/player.js
+++ src/routes/api/player.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import bodyParser from 'body-parser';
 
-import { writeCommandToFifo, stopPianoBar, readCurrentSong, startPianoBar } from '../../services/pianobar';
+import { writeCommandToFifo, stopPianoBar, startPianoBar } from '../../services/pianobar';
 import { socketBroadcast } from '../../services/socketFunctions';
 
 export var isPaused = false;
@@ -11,84 +11,61 @@ var playerRouter = Router();
 playerRouter.use(bodyParser.json());
 
 /* GET users listing. */
-playerRouter.get('/', function (req, res, next) {
-    let playerState = {
-        playerRunning,
-        isPaused
-    };
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(playerState);
-});
-
-playerRouter.post('/', (req, res, next) => {
-    const validCommands = {
-        VOLUME_UP: ')',
-        VOLUME_DOWN: '(',
-        PLAYPAUSE: 'p',
-        NEXT: 'n',
-        STOPPLAYER: 'STOPPLAYER',
-        STARTPLAYER: 'STARTPLAYER'
-    }
-    console.log('Got command ' + req.query.command);
-    if (Object.keys(validCommands).includes(req.query.command)) {
-        console.log('Starting write to file');
-        const action = validCommands[req.query.command];
-        if (action === validCommands.STOPPLAYER) {
-            stopPianoBar((error, stdout, stderr) => {
-                if (error) {
-                    return next(error);
-                }
-                playerRunning = false;
-                isPaused = false;
-                socketBroadcast('player');
-                res.status = 200;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Successfully terminated PianoBar');
-            });
-        } else if (action === validCommands.STARTPLAYER) {
-            startPianoBar((error, stdout, stderr) => {
-                if (error) {
-                    return next(error);
-                }
-                playerRunning = true;
-                isPaused = false;
-                socketBroadcast('player');
-                res.status = 200;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Successfully started PianoBar');
-            });
-        } else {
-            if (!playerRunning) {
-                var error = new Error('No player currently running.  Please start player and try again')
-                return next(error);
-            }
-
-            writeCommandToFifo(action)
-                .then((written, error) => {
-                    if (error) {
-                        return next(error);
-                    }
-                    if (written.bytesWritten == action.length) {
-                        if (action === validCommands.PLAYPAUSE) {
-                            isPaused = !isPaused;
-                            socketBroadcast('isPaused');
-                        }
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'text/plain');
-                        res.end(action + ' has been written successfully!');
-
-                    } else {
-                        error = new Error('Error: Only wrote ' + written.bytesWritten + ' out of ' + action.length + ' bytes to fifo. \n Wrote: ' + written.buffer);
-                        return next(error);
-                    }
-                })
-                .catch(error => next(error));
+playerRouter.route('/')
+    .get(function (req, res, next) {
+        let playerState = {
+            playerRunning,
+            isPaused
+        };
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(playerState);
+    })
+    .post((req, res, next) => {
+        let response;
+        const validCommands = {
+            VOLUME_UP: ')',
+            VOLUME_DOWN: '(',
+            PLAYPAUSE: 'p',
+            NEXT: 'n',
+            STOPPLAYER: 'STOPPLAYER',
+            STARTPLAYER: 'STARTPLAYER'
         }
-    } else {
-        var err = new Error('Invalid command: ' + req.query.command + ', please select from the following commands:\n' + Object.keys(validCommands).join('\n'));
-        return next(err);
-    }
-});
+        console.log('Got command ' + req.query.command);
+        if (Object.keys(validCommands).includes(req.query.command)) {
+            console.log('Starting write to file');
+            const action = validCommands[req.query.command];
+            try {
+                if (action === validCommands.STOPPLAYER) {
+                    await stopPianoBar();
+                    playerRunning = false;
+                    isPaused = false;
+                    socketBroadcast('player');
+                    response = 'Successfully terminated PianoBar';
+                } else if (action === validCommands.STARTPLAYER) {
+                    await startPianoBar();
+                    playerRunning = true;
+                    isPaused = false;
+                    socketBroadcast('player');
+                    response = 'Successfully started PianoBar';
+                } else {
+                    await writeCommandToFifo(action);
+                    if (action === validCommands.PLAYPAUSE) {
+                        isPaused = !isPaused;
+                        socketBroadcast('isPaused');
+                    }
+                    response = action + ' has been written successfully!';
+                }
+                res.status = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end(response);
+            } catch (error) {
+                next(error);
+            }
+        } else {
+            var error = new Error('Invalid command: ' + req.query.command + ', please select from the following commands:\n' + Object.keys(validCommands).join('\n'));
+            next(error);
+        }
+    });
 
 export default playerRouter;
--- src/services/pianobar.js
+++ src/services/pianobar.js
@@ -1,58 +1,80 @@
 import fs from 'fs';
 import { exec } from 'child_process';
+import { playerRunning } from '../routes/api/player'
 
 //var fifo = process.env.PIANOBAR_FIFO || '/home/pi/Patiobar/ctl';
 var fifo = '/Blankenship/github/audiopiServer/public/ctl';
-export const writeCommandToFifo = (action) =>  {
-    return fs.promises.open(fifo, 'w', 0o644)
-        .then((fileHandle, error) => {
-            console.log('Fifo opened');
-            if (error) {
-                console.log('Error opening fifo: ' + error);
-                return error;
-            }
 
-            var buf = new Buffer.from(action);
+export const writeCommandToFifo = async (action) => {
+    let error;
 
-            return fileHandle.write(buf, 0, action.length, null);
-        })
-        .catch((error) => error);
+    if (!playerRunning) {
+        error = new Error('No player currently running.  Start player before sending commands.');
+        throw error;
+    }
+    const fileHandle = await fs.promises.open(fifo, 'w', 0o644);
+    console.log('Fifo opened');
+
+    var buf = new Buffer.from(action);
+
+    const written = await fileHandle.write(buf, 0, action.length, null);
+    if (written.bytesWritten == action.length) {
+        return written;
+    } else {
+        error = new Error('Error: Only wrote ' + written.bytesWritten + ' out of ' + action.length + ' bytes to fifo. \n Wrote: ' + written.buffer);
+        throw error;
+    }
 }
 
-export const readCurrentSong = () => {
+export const readCurrentSong = async () => {
     //const path = process.env.HOME + '/.config/pianobar/currentSong';
     const path = '/Blankenship/github/audiopiServer/public/currentSong';
-    return fs.promises.open(path,'r')
-        .then((fileHandle, error) => {
-            console.log('reading current song')
-            if (error) {
-                return error;
-            }
-            return fileHandle.readFile();
-        })
-        .catch((error) => error);
-}
+    const fileHandle = await fs.promises.open(path, 'r');
+    console.log('reading current song')
+    const currentSong = await fileHandle.readFile();
+    const currentSongString = currentSong.toString();
+    if (currentSongString) {
+        var songData = currentSongString.split(',,,');
+        return {
+            artist: songData[0],
+            title: songData[1],
+            album: songData[2],
+            coverArt: songData[3],
+            rating: songData[4],
+            stationName: songData[5]
+        };
+    }
+    let error = new Error('No current song data available.');
+    throw error;
+};
 
-export const readStations = () => {
+export const readStations = async () => {
     //const path = process.env.HOME + '/.config/pianobar/stationList';
     const path = '/Blankenship/github/audiopiServer/public/stationList';
-    return fs.promises.open(path,'r')
-        .then((fileHandle, error) => {
-            console.log('reading station list')
-            if (error) {
-                return error;
-            }
-            return fileHandle.readFile();
-        })
-        .catch((error) => error);
-}
+    const fileHandle = await fs.promises.open(path, 'r')
+    console.log('reading station list')
+    const stationList = await fileHandle.readFile();
+    const stationListString = stationList.toString();
+    if (stationListString) {
+        const stationData = stationListString.split('\n');
+        return stationData.map((station) => {
+            const stationArray = station.split(":");
+            return {
+                stationId: stationArray[0],
+                stationName: stationArray[1].replace('\r', '')
+            };
+        });
+    }
+    let error = new Error('No station data available.');
+    throw error;
+};    
 
-export const startPianoBar = (callBack) => {
+export const startPianoBar = async () => {
     //exec('bash /home/pi/Patiobar/scripts/pbstart.sh', callBack);
-    exec('bash /h/Blankenship/github/audiopiServer/public/pbStart.sh', callBack)
+    await exec('bash /h/Blankenship/github/audiopiServer/public/pbStart.sh');
 }
 
-export const stopPianoBar = (callBack) => {
+export const stopPianoBar = () => {
     //exec('bash /home/pi/Patiobar/scripts/pbstop.sh', callBack);
-    exec('bash /h/Blankenship/github/audiopiServer/public/pbStop.sh', callBack)
+    await exec('bash /h/Blankenship/github/audiopiServer/public/pbStop.sh');
 }
