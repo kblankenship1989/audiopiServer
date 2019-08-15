59fed992f95c65c783a7643b105885522c68c953
Kurtis Blankenship <BlankenshipKurtisA@JohnDeere.com>
2019-08-15 08:39:52 -0500

    fixes for media query

--- dist/precache-manifest.fd25c1edaa8cfbbfead40c3f1e0cc5e8.js
+++ dist/precache-manifest.fd25c1edaa8cfbbfead40c3f1e0cc5e8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "af4742f9765f9e77738b94c8ff573267",
+    "revision": "d11194019e64e93abecd688547c2fb7b",
     "url": "/index.html"
   },
   {
-    "revision": "b910e7ed38fc2f6ae458",
+    "revision": "ca259e53ae9462a8648d",
     "url": "/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "6a51a06efd9373e7d572",
+    "revision": "fdcf9c44b27652c600aa",
     "url": "/static/css/main.278c60ef.chunk.css"
   },
   {
-    "revision": "b910e7ed38fc2f6ae458",
-    "url": "/static/js/2.cf149133.chunk.js"
+    "revision": "ca259e53ae9462a8648d",
+    "url": "/static/js/2.5058a3f3.chunk.js"
   },
   {
-    "revision": "6a51a06efd9373e7d572",
-    "url": "/static/js/main.936c5429.chunk.js"
+    "revision": "fdcf9c44b27652c600aa",
+    "url": "/static/js/main.5c343d80.chunk.js"
   },
   {
     "revision": "aca7b42372bd08e9824e",
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73db117afb8baaddf77827bf52013bab.js"
+  "/precache-manifest.fd25c1edaa8cfbbfead40c3f1e0cc5e8.js"
 );
 
 self.addEventListener('message', (event) => {
