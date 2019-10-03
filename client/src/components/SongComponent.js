20a3bdb1033151d1792c8e59e933a7715509e1c0
Kurtis Blankenship <kblankenship1989@gmail.com>
2019-10-02 21:31:47 -0500

    updating styles for image

--- client/src/components/SongComponent.js
+++ client/src/components/SongComponent.js
@@ -64,7 +64,6 @@ export const SongControls = (props) => {
 	};
 	
 	const songPlayed = Math.round(100*parseInt(currentSong.currentSong.songPlayed)/parseInt(currentSong.currentSong.songDuration));
-	console.log(songPlayed);
 	
 	const handleClick = (command) => {
 		console.log("Execute Pianobar command: " + command);
@@ -73,34 +72,25 @@ export const SongControls = (props) => {
 	}
 
     return(
-        <Col md="6 m-auto" className="song-controls">
+        <Col className="song-controls">
 			<CoverFlow
-				displayQuantityOfSide={2}
+				displayQuantityOfSide={1}
 				navigation={true}
 				active={songHistory.length}
-				width="auto"
 				enableScroll={true}
+				enableHeading={true}
+				width="100%"
+				height="600"
 			>
-				{songHistory.map((song) => {
+				{songHistory.reverse().map((song) => {
 					return (
-						<div key={song.title}>
-							<Media src={song.coverArt} alt={song.title} className="col-12 m-auto"/>
-							<Media bottom body>
-								<Media heading>{song.title}</Media>
-								{song.artist}
-							</Media>
-						</div>
+						<img src={song.coverArt} alt={song.title} width="100%" height="100%" style={{
+							maxHeight: 400,
+							maxWidth:400,
+							alignContent: 'center'
+						}} mode='fit'/>
 					);
 				})}
-				<div key={currentSong.currentSong.title}>
-					<Media src={currentSong.currentSong.coverArt} alt={currentSong.currentSong.title} className="col-12 m-auto"/>
-					<Media>
-						<Media bottom body>
-							<Media heading>{currentSong.currentSong.title}</Media>
-							{currentSong.currentSong.artist}
-						</Media>
-					</Media>
-				</div>
 			</CoverFlow>
             <br />
             <Progress value={songPlayed} />
--- client/src/redux/pandora.js
+++ client/src/redux/pandora.js
@@ -7,7 +7,48 @@ const emptyState = {
         currentSong: {}
     },
     stationList: [],
-    songHistory: []
+    songHistory: [
+        {
+            artist: "Philip Wesley",
+            title: "New Day",
+            album: "Dark Night Of The Soul",
+            coverArt: "http://mediaserver-cont-sv5-2-v4v6.pandora.com/images/public/int/3/2/2/2/619981272223_500W_500H.jpg",
+            rating: 0,
+            stationName: "Indignation Radio"
+        },
+        {
+            artist: "Philip Wesley",
+            title: "New Day",
+            album: "Dark Night Of The Soul",
+            coverArt: "http://mediaserver-cont-sv5-2-v4v6.pandora.com/images/public/int/3/2/2/2/619981272223_500W_500H.jpg",
+            rating: 0,
+            stationName: "Indignation Radio"
+        },
+        {
+            artist: "Philip Wesley",
+            title: "New Day",
+            album: "Dark Night Of The Soul",
+            coverArt: "http://mediaserver-cont-sv5-2-v4v6.pandora.com/images/public/int/3/2/2/2/619981272223_500W_500H.jpg",
+            rating: 0,
+            stationName: "Indignation Radio"
+        },
+        {
+            artist: "Philip Wesley",
+            title: "New Day",
+            album: "Dark Night Of The Soul",
+            coverArt: "http://mediaserver-cont-sv5-2-v4v6.pandora.com/images/public/int/3/2/2/2/619981272223_500W_500H.jpg",
+            rating: 0,
+            stationName: "Indignation Radio"
+        },
+        {
+            artist: "Philip Wesley",
+            title: "New Day",
+            album: "Dark Night Of The Soul",
+            coverArt: "http://mediaserver-cont-sv5-2-v4v6.pandora.com/images/public/int/3/2/2/2/619981272223_500W_500H.jpg",
+            rating: 0,
+            stationName: "Indignation Radio"
+        }
+    ]
 };
 
 export const Pandora = (state = emptyState, action) => {
