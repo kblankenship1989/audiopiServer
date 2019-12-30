7dba211f8837f8612489fc124f4943af7024ac5d
Kurtis Blankenship <kblankenship1989@gmail.com>
2019-12-30 10:13:22 -0600

    fixing set relay function

--- src/classes/Relay.js
+++ src/classes/Relay.js
@@ -8,14 +8,14 @@ export class Relay {
     }
 
     setRelays(newState) {
-        latch.writeSync(0);
-        clock.writeSync(0);
+        this.latch.writeSync(0);
+        this.clock.writeSync(0);
         let j=0;
         for (j=0;j<16;j++){
-            data.writeSync((newState >> j) & 1);
-            clock.writeSync(1);
-            clock.writeSync(0);
+            this.data.writeSync((newState >> j) & 1);
+            this.clock.writeSync(1);
+            this.clock.writeSync(0);
         }
-        latch.writeSync(1);
+        this.latch.writeSync(1);
     }
 }
