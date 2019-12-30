878b3e227fd0d20e59a96eca021fe842da50e988
Kurtis Blankenship <kblankenship1989@gmail.com>
2019-12-30 10:11:01 -0600

    correcting typo

--- src/services/settings.js
+++ src/services/settings.js
@@ -2,7 +2,7 @@
 import {readFileSync, writeFile} from 'fs';
 import {join} from 'path';
 
-const filePath = join(__dirname, '../../public/stationList');
+const filePath = join(__dirname, '../../public/settings.json');
 const encoding = 'utf8';
 
 const settingsRaw = readFileSync(filePath, encoding);
