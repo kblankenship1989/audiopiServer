18c84993e73b043b9a03311f6c43dbffdecc3fb7
Kurtis Blankenship <kblankenship1989@gmail.com>
2019-12-30 11:16:09 -0600

    errors with update settings

--- src/services/settings.js
+++ src/services/settings.js
@@ -3,14 +3,14 @@ import {readFileSync, writeFile} from 'fs';
 import {join} from 'path';
 
 const filePath = join(__dirname, '../../public/settings.json');
-const encoding = 'utf8';
 
-const settingsRaw = readFileSync(filePath, encoding);
+const settingsRaw = readFileSync(filePath);
 export const settings = JSON.parse(settingsRaw);
 
 export const updateSetting = (key, value, callback) => {
     var noop = function(){};
     const callbackToExecute = callback || noop;
     settings[key] = value;
-    writeFile(filePath, JSON.stringify(settings), encoding, callbackToExecute);
+    console.log(JSON.stringify(settings));
+    writeFile(filePath, JSON.stringify(settings), callbackToExecute);
 }
