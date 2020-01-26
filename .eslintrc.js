ebdc0a319b2c364f536a0a87c061dfd1ca92b9fd
Kurtis Blankenship <kblankenship1989@gmail.com>
2020-01-25 23:11:29 -0600

    starting tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "jest/globals": true,
+        "node": true,
+        "commonjs": true
     },
     "extends": [
         "eslint:recommended",
@@ -19,7 +22,8 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
+        "jest"
     ],
     "rules": {
     }
